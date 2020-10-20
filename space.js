6d7c6a045e61150a6b71bb7ee2e8e442574da1f8
Henry Okonkwo <okonkwo.henry2012@gmail.com>
2020-10-20 23:16:08 +0200

    fetch All Starlink creates a Fetch all starlink function store response in runtime memory adds unit test

--- space.js
+++ space.js
@@ -1,10 +1,10 @@
 import axios from "axios";
+import { database } from "./runtimeMemory";
 
 class SpaceX {
   constructor(url) {
     this.url = url;
   }
-
   async fetchALauchpad(launchpadId) {
     if (launchpadId) {
       return await axios
@@ -12,6 +12,16 @@ class SpaceX {
         .then((response) => response)
         .catch((error) => error);
     }
+  }
+
+  async FetchAllStarlink() {
+    return await axios
+      .get(this.url)
+      .then((response) => {
+        database.push({ ...response });
+        return response;
+      })
+      .catch((error) => error);
   }
 }
 
--- space.spec.js
+++ space.spec.js
@@ -1,24 +1,42 @@
 import SpaceX from "./space";
-import { lanchPadById } from "./mockObject";
+import { lanchPadObject, starLinksSateliteArray} from "./mockObject";
 import axios from "axios";
 
 jest.mock("axios");
 
 describe("SpaceX", () => {
-  it("should return a single lauchpad object if correct ID is provided", async () => {
-    await axios.get.mockImplementationOnce(() => Promise.resolve(lanchPadById));
+
+  it("should fetch a single lauchpad object if correct ID is provided", async () => {
+    await axios.get.mockImplementationOnce(() => Promise.resolve(lanchPadObject));
     const spaceUrl = new SpaceX("https://api.spacexdata.com/v4/launchpads");
     expect(
       spaceUrl.fetchALauchpad("5e9e4502f5090995de566f86")
-    ).resolves.toEqual(lanchPadById);
+    ).resolves.toEqual(lanchPadObject);
   });
 
   it("should return error message if wrong ID is provided", async () => {
     const errorMessage = "Not Found";
     await axios.get.mockImplementationOnce(() => Promise.reject(errorMessage));
     const spaceUrl = new SpaceX("https://api.spacexdata.com/v4/launchpads");
-    expect(spaceUrl.fetchALauchpad("wrongid")).resolves.toEqual(
+    expect(spaceUrl.fetchALauchpad("wrongId")).resolves.toEqual(
       errorMessage
     );
   });
+
+  it("should fetch all starlinks satelights giving the correct url", async () => {
+    const database = []
+    await axios.get.mockImplementationOnce(() => Promise.resolve(starLinksSateliteArray));
+    const starlinksUrl = new SpaceX("https://api.spacexdata.com/v4/starlink");
+    database.push({...starLinksSateliteArray})
+    expect(starlinksUrl.FetchAllStarlink()).resolves.toEqual(starLinksSateliteArray);
+  });
+
+  it("should return error message if wrong url is provided", async () => {
+    const errorMessage = "Not Found";
+    await axios.get.mockImplementationOnce(() => Promise.reject(errorMessage));
+    const starlinksUrl = new SpaceX("https://api.spacexdata.com/v4/wronglink");
+    expect(starlinksUrl.FetchAllStarlink()).resolves.toEqual(errorMessage);
+  });
+
+  
 });
